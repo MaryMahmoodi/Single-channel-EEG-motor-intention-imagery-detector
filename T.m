9bbce4317f8304365f5a82a931e4c4fab0f05419
MaryMahmoodi <72376694+MaryMahmoodi@users.noreply.github.com>
2021-11-08 07:14:54 +0330

    Add files via upload

---
+++ T.m
@@ -0,0 +1,13 @@
+function [ y ] = T( x )
+%applies the teager operator, and returns the output
+%y = T(x)
+
+for i = 2:length(x)-1
+    y(i) = x(i)^2 - x(i-1)*x(i+1);
+omega=acos((x(i-1)+x(i+1))/(2*x(i)));
+a=(sinc(omega)).^2;
+y(i)=y(i);%./a;
+end
+y=[0 y];
+end
+
